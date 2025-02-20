4d0ec347e27effde23974cafd724518469c3615a
Ethan Chien <liangtie.qian@gmail.com>
2025-02-20 23:23:36 +0800

    fix: Update copyright year in assistant and updater source files

--- copilot/assistant/assistant_launcher.cpp
+++ copilot/assistant/assistant_launcher.cpp
@@ -1,8 +1,8 @@
 /*
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
- * Copyright (C) 2023 Ethan Chien <liangtie.qian@gmail.com>
- * Copyright (C) 2023 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2025 Ethan Chien <liangtie.qian@gmail.com>
+ * Copyright (C) 2025 KiCad Developers, see AUTHORS.txt for contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
